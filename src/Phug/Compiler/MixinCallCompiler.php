aa979ee2f3de0b161d7099629d177f2a6e9bee77
KyleKatarn <kylekatarnls@gmail.com>
2017-06-27 14:47:59 +0200

    Handle assignments on mixin calls

--- src/Phug/Compiler/MixinCallCompiler.php
+++ src/Phug/Compiler/MixinCallCompiler.php
@@ -5,11 +5,10 @@ namespace Phug\Compiler;
 use Phug\AbstractNodeCompiler;
 use Phug\Ast\NodeInterface;
 use Phug\CompilerException;
-use Phug\Formatter\Element\CodeElement;
 use Phug\Formatter\Element\DocumentElement;
 use Phug\Formatter\Element\ExpressionElement;
 use Phug\Formatter\ElementInterface;
-use Phug\Parser\Node\AttributeNode;
+use Phug\Parser\Node\CodeNode;
 use Phug\Parser\Node\MixinCallNode;
 use Phug\Parser\Node\MixinNode;
 use Phug\Parser\NodeInterface as ParserNodeInterface;
@@ -48,12 +47,28 @@ class MixinCallCompiler extends AbstractNodeCompiler
             $store = is_null($attribute->getName()) ? 'arguments' : 'attributes';
             array_push($$store, $attribute);
         }
-        $attributes = '['.implode(', ', array_map(function (AttributeNode $attribute) {
-            return var_export($attribute->getName(), true).' => '.$attribute->getValue();
-        }, $attributes)).']';
         $variables = [
-            'attributes' => new ExpressionElement($attributes),
+            'attributes' => count($attributes)
+                ? $compiler->getFormatter()->formatAttributesList(array_map(function ($node) use ($compiler) {
+                    return $compiler->compileNode($node);
+                }, $attributes))
+                : new ExpressionElement('[]'),
         ];
+        $mergeAttributes = [];
+        foreach ($node->getAssignments() as $assignment) {
+            if ($assignment->getName() === 'attributes') {
+                foreach ($assignment->getAttributes() as $attribute) {
+                    $mergeAttributes[] = $compiler->getFormatter()->formatCode($attribute->getValue());
+                }
+            }
+        }
+        if (count($mergeAttributes)) {
+            $variables['attributes']->setValue(sprintf(
+                'array_merge(%s, %s)',
+                $variables['attributes']->getValue(),
+                implode(', ', $mergeAttributes)
+            ));
+        }
         foreach ($declaration->getAttributes() as $index => $attribute) {
             $variables[$attribute->getName()] = new ExpressionElement(
                 isset($arguments[$index]) ? $arguments[$index]->getValue() : 'null'
@@ -72,8 +87,8 @@ class MixinCallCompiler extends AbstractNodeCompiler
         foreach ($declaration->getChildren() as $child) {
             $document->appendChild(clone $child);
         }
+        $node->appendChild(new CodeNode('extract($'.$scopeName.')'));
         $this->proceedBlocks($document, $this->getCompiledChildren($node, $parent));
-        $document->appendChild(new CodeElement('extract($'.$scopeName.')'));
 
         return $document;
     }
