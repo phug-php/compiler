0bd26890a803e71b357ad1b61335a285c9f9020f
KyleKatarn <kylekatarnls@gmail.com>
2017-07-09 11:48:09 +0200

    Ensure exception code is an integer

--- src/Phug/CompilerException.php
+++ src/Phug/CompilerException.php
@@ -12,11 +12,11 @@ class CompilerException extends \Exception implements PugFileLocationInterface
 {
     use PugFileLocationTrait;
 
-    public function __construct($message = "", $code = 0, $previous = null, $file = null, $line = null, $offset = null)
+    public function __construct($message = '', $code = 0, $previous = null, $file = null, $line = null, $offset = null)
     {
         $this->setPugFile($file);
         $this->setPugLine($line);
         $this->setPugOffset($offset);
-        parent::__construct($message, $code, $previous);
+        parent::__construct($message, intval($code), $previous);
     }
 }
